284ab762a6e6753987a16b736c5b459274b0f222
Esteban <esgaanza98@gmail.com>
2020-08-02 18:55:15 +1000

    Added Basic conversion struct

--- week 1/convertRGBtoGrayscale.m
+++ week 1/convertRGBtoGrayscale.m
@@ -10,7 +10,8 @@ for i = 1:rows
     for j = 1:cols
         % Your logic goes in here
           % greyscale value = 0.2989 * R + 0.5870 * G + 0.1140 * B
-        imgGS(i,j)= 0.2989 * imgRGB(i,j,1) + 0.5870 * imgRGB(i,j,2) + 0.1140 * imgRGB(i,j,3);
+          %imgGS(i,j)=(imgGS(i,j,1)+imgGS(i,j,2)+imgGS(i,j,3))/3;
+          imgGS(i,j)= 0.2989 * imgRGB(i,j,1) + 0.5870 * imgRGB(i,j,2) + 0.1140 * imgRGB(i,j,3);
     end
 end
 
